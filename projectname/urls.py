04af36b879bdf2129030919b2e99d124b393524e
potturuvenkatesh <139435969+potturuvenkatesh@users.noreply.github.com>
2023-08-03 11:21:40 +0530

    Update urls.py

--- projectname/urls.py
+++ projectname/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('appname.urls')),
     path('Dhanalaxshmi/', include('Dhanalaxshmi.urls')),
+     path('Business', include('Business.urls')),
 ]
